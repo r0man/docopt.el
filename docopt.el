1fd2eaf1fb56032a95a567f2dc669f25534f77d3
r0man <roman@burningswell.com>
2020-02-06 21:15:20 +0100

    Move example to tests

--- docopt.el
+++ docopt.el
@@ -102,25 +102,6 @@ slots of the instance."
                       :description description
                       :name short-name)))))
 
-(defvar docopt-example "
-Naval Fate.
-
-Usage:
-  naval_fate ship new <name>...
-  naval_fate ship <name> move <x> <y> [--speed=<kn>]
-  naval_fate ship shoot <x> <y>
-  naval_fate mine (set|remove) <x> <y> [--moored|--drifting]
-  naval_fate -h | --help
-  naval_fate --version
-
-Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  --speed=<kn>  Speed in knots [default: 10].
-  --moored      Moored (anchored) mine.
-  --drifting    Drifting mine.
-")
-
 (defun docopt--parse-whitespace ()
   "Parse spaces and newlines."
   (parsec-re "[[:space:]\r\n]"))
