73d43ea22b15caed328668f5171c0d5ed6ad6148
r0man <roman@burningswell.com>
2020-02-29 18:54:41 +0100

    Add license

--- docopt.el
+++ docopt.el
@@ -4,7 +4,28 @@
 ;; Keywords: docopt
 ;; Version: 0.1.0
 
-;; This file is NOT part of GNU Emacs.
+;; Copyright (C) 2019-2020 r0man
+
+;; Author: r0man <roman@burningswell.com>
+;; Maintainer: r0man <roman@burningswell.com>
+;; Created: 29 Feb 2020
+;; Keywords: docopt, command line argument
+;; Homepage: https://github.com/r0man/docopt.el
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses.
 
 ;;; Commentary:
 
--- test/docopt-testcase-test.el
+++ test/docopt-testcase-test.el
@@ -1,5 +1,28 @@
 ;;; docopt-testcase-test.el --- The Docopt testcase tests -*- lexical-binding: t -*-
 
+;; Copyright (C) 2019-2020 r0man
+
+;; Author: r0man <roman@burningswell.com>
+;; Maintainer: r0man <roman@burningswell.com>
+;; Created: 29 Feb 2020
+;; Keywords: docopt, command line argument
+;; Homepage: https://github.com/r0man/docopt.el
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses.
+
 ;;; Commentary:
 
 ;; The Docopt testcase tests
