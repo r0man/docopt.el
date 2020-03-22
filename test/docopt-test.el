d88b5908bc570e6804aa7c08f43f109ffda9eb60
r0man <roman@burningswell.com>
2020-03-22 21:05:53 +0100

    Fix test #16

--- test/docopt-test.el
+++ test/docopt-test.el
@@ -95,7 +95,7 @@ Options:
 ;; (seq-doseq (testcase (docopt-parse-testcases (f-read-text "test/testcases.docopt")))
 ;;   (docopt-test-define-describe testcase))
 
-(seq-doseq (testcase (seq-take (docopt-parse-testcases (f-read-text "test/testcases.docopt")) 15))
+(seq-doseq (testcase (seq-take (docopt-parse-testcases (f-read-text "test/testcases.docopt")) 16))
   (docopt-test-define-describe testcase))
 
 ;; (setq my-testcase (nth 5 (docopt-parse-testcases (f-read-text "test/testcases.docopt"))))
