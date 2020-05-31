57441cb907414d18d2ef378167cf66e010f0329e
r0man <roman@burningswell.com>
2020-05-31 22:09:36 +0200

    Add CI

--- docopt-pkg.el
+++ docopt-pkg.el
@@ -32,10 +32,12 @@
 
 (define-package "docopt" "0.1.0"
   "A Docopt implementation in Elisp"
-  '((cl-lib "1.0")
+  '((buttercup "1.21")
+    (cl-lib "1.0")
     (dash "2.17.0")
     (emacs "26.1")
     (f "0.20.0")
+    (let-alist "1.0.6")
     (parsec "0.1.3")
     (s "1.12.0")
     (seq "2.21")
