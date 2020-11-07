c8564bdf0431d1d6a0279e0eaaa524e1b2cec7a6
r0man <roman@burningswell.com>
2020-11-07 16:29:46 +0100

    Move package require to docopt.el

--- src/docopt.el
+++ src/docopt.el
@@ -5,6 +5,7 @@
 ;; Author: r0man <roman@burningswell.com>
 ;; Maintainer: r0man <roman@burningswell.com>
 ;; Keywords: docopt, tools, processes
+;; Package-Requires: ((emacs "26.3") (dash "2.17.0") (emacs "26.1") (f "0.20.0") (let-alist "1.0.6") (parsec "0.1.3") (s "1.12.0") (transient "0.3.0"))
 ;; Homepage: https://github.com/r0man/docopt.el
 ;; Version: 0.1.0
 
